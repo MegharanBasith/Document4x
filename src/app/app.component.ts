419addd8eb0bd5646b8101c65d251d90d75cf5ee
Basith <m.basith@faaztechsolutions.com>
2024-09-03 10:39:38 +0530

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,15 @@ import { NavigationStart, Router } from '@angular/router';
 export class AppComponent implements OnInit {
   title = 'Document';
   constructor(private router: Router) {
-    router.events.subscribe((event) => {
-      if (event instanceof NavigationStart) {
-        debugger;
-        if (event.url != '/' && event.url != '' &&JSON.parse(sessionStorage.getItem('CurrentUser')!) != '') 
-        {
-          this.router.navigate(['']);
-        }
-      }
-    });
+    // router.events.subscribe((event) => {
+    //   if (event instanceof NavigationStart) {
+    //     debugger;
+    //     if (event.url != '/' && event.url != '' &&JSON.parse(sessionStorage.getItem('CurrentUser')!) != '') 
+    //     {
+    //       this.router.navigate(['']);
+    //     }
+    //   }
+    // });
   }
   ngOnInit(): void {}
 }
