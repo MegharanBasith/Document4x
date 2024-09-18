2f7f543a661befb3b77eb31062dc094f0d5105cb
Basith <m.basith@faaztechsolutions.com>
2024-09-18 19:30:43 +0530

    app component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,15 @@ import { NavigationStart, Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   title = 'Document';
-  constructor(private router: Router) {
-    // router.events.subscribe((event) => {
-    //   if (event instanceof NavigationStart) {
-    //     debugger;
-    //     if (event.url != '/' && event.url != '' &&JSON.parse(sessionStorage.getItem('CurrentUser')!) != '') 
-    //     {
-    //       this.router.navigate(['']);
-    //     }
-    //   }
-    // });
-  }
+  constructor(private route:Router) {
+    let pageLoad = sessionStorage.getItem("isLoggedIn")!;
+    if((pageLoad!=null || pageLoad!=undefined) && pageLoad=="true"){
+     this.isVisible=true;
+    }
+    else{
+     this.route.navigate(['/'])
+    }
+   }
   ngOnInit(): void {}
+  isVisible:boolean = false;
 }
