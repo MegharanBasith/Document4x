96d499ce51e092002617ffdcf536eacb0bf34d26
Basith <m.basith@faaztechsolutions.com>
2024-09-23 17:37:48 +0530

    revert changes

--- src/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard.component.ts
@@ -12,8 +12,7 @@ import { Router } from '@angular/router';
 export class DashboardComponent {
   constructor(private route:Router ) {
     let data = sessionStorage.getItem("childMenu")!;
-    this.childMenu = JSON.parse(data).setTimeout(() => {
-    }, 1000);; 
+    this.childMenu = JSON.parse(data)
   }
   childMenu: any;
   openTileLink(data:any){
